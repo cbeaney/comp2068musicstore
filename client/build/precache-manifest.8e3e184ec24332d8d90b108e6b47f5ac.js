f905922206ea4c33a7e026c151e1145bb2982578
cbeaney <200376623@student.georgianc.on.ca>
2019-08-12 16:30:56 -0400

    shaun fixed it!!!!! how could we have known????

---
+++ client/build/precache-manifest.8e3e184ec24332d8d90b108e6b47f5ac.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9bc594184e57d079e45719b7115b34d1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3d707f2d8495a1fe2c0b",
+    "url": "/static/js/2.1130137e.chunk.js"
+  },
+  {
+    "revision": "6ad87c217ef953315360",
+    "url": "/static/js/main.c0bdc359.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
